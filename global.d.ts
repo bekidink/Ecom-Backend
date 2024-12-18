683a63b25f668eb5a9248b2db05dfae40203c04e
bekidink <bereketdinku876@gmail.com>
2024-12-18 12:21:19 -0800

    updated

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+declare module "express";
+declare module "cors";
+declare module "morgan";
+declare module "cookie-parser";
+declare module "jsonwebtoken";
+declare module "bcryptjs";
+declare module "multer";
